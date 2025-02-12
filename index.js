287ace8321b03188b8c5392728775408027f2ac9
Chalongmongkol Wankasem <wankasem_c@silpakorn.edu>
2025-02-12 23:27:57 +0700

    Gameplay
    can play game

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
 var round = 0
 var winscore = 0
 var losescore = 0
+var alwaysscore = 0
+// 0 ค้อน   1 กรรไกร    2 กระดาษ
 
-function addscore(result){
-    if(result=="Win"){
-        winscore += 1
-    }else{
-        losescore += 1
-    } 
+function addRound(){
     round += 1
 }
 
-document.getElementById("score").innerHTML = "round : "+round+"  win :"+winscore+"  lose :"+losescore 
+function playGame(game){
+    let bot = Math.floor(Math.random()*3)
+    if(game=="0"&&bot==1){
+        winscore += 1
+    }else if(game=="1"&&bot==2){
+        winscore += 1
+    }else if(game=="2"&&bot==0){
+        winscore += 1
+    }else if(game==bot){
+        alwaysscore += 1
+    }else{
+        losescore += 1
+    } 
+    addRound()
+    document.getElementById("score").innerHTML = "Round : "+round+"  Win :"+winscore+"  Lose :"+losescore+"   Always : "+alwaysscore
+}
+
+document.getElementById("score").innerHTML = "Round : "+round+"  Win :"+winscore+"  Lose :"+losescore+"   Always : "+alwaysscore
