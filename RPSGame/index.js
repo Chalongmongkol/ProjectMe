529d39f6e3e62388f6bdc4e1d670b9a140b001f4
Chalongmongkol Wankasem <wankasem_c@silpakorn.edu>
2025-02-14 00:12:25 +0700

    Add Home

---
+++ RPSGame/index.js
@@ -0,0 +1,64 @@
+var round = 0
+var winscore = 0
+var losescore = 0
+var alwaysscore = 0
+
+// 0 ค้อน   1 กรรไกร    2 กระดาษ
+
+function closeModal(){
+    document.getElementById("modal").style.display = "none";
+}
+
+function openModal(){
+    document.getElementById("modal").style.display = "flex";
+}
+
+function addRound(){
+    round += 1
+}
+
+function setplay(bot,game){
+    if(game==0){
+        document.getElementById("me").innerHTML = "<img src='/picture/rock.png' class='game-img'>"
+    }else if(game==1){
+        document.getElementById("me").innerHTML = "<img src='/picture/scissors.png' class='game-img'>"
+    }else{
+        document.getElementById("me").innerHTML = "<img src='/picture/paper.png' class='game-img'>"
+    }
+    if(bot==0){
+        document.getElementById("bot").innerHTML = "<img src='/picture/rock.png' class='game-img'>"
+    }else if(bot==1){
+        document.getElementById("bot").innerHTML = "<img src='/picture/scissors.png' class='game-img'>"
+    }else{
+        document.getElementById("bot").innerHTML = "<img src='/picture/paper.png' class='game-img'>"
+    }
+}
+
+
+function playGame(game){
+    let bot = Math.floor(Math.random()*3)
+    if(game=="0"&&bot==1){
+        winscore += 1
+        document.getElementById("result").innerHTML = "Win"
+        
+    }else if(game=="1"&&bot==2){
+        winscore += 1
+        document.getElementById("result").innerHTML = "Win"
+    }else if(game=="2"&&bot==0){
+        winscore += 1
+        document.getElementById("result").innerHTML = "Win"
+    }else if(game==bot){
+        alwaysscore += 1
+        document.getElementById("result").innerHTML = "Draw"
+    }else{
+        losescore += 1
+        document.getElementById("result").innerHTML = "lose"
+    } 
+    setplay(bot,game)
+    addRound()
+   
+document.getElementById("score").innerHTML = "Scoreboard<br>Round : "+round+"<br>Win : "+winscore+"<br>Lose : "+losescore+"<br>Draw : "+alwaysscore
+}
+
+document.getElementById("score").innerHTML = "Scoreboard<br>Round : "+round+"<br>Win : "+winscore+"<br>Lose : "+losescore+"<br>Draw : "+alwaysscore
+document.getElementById("result").innerHTML = "?"
