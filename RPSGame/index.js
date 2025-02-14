67d4238e9aee96e30ea24d66def889ce80fdbfdb
Chalongmongkol Wankasem <wankasem_c@silpakorn.edu>
2025-02-14 11:50:50 +0700

    update history

--- RPSGame/index.js
+++ RPSGame/index.js
@@ -2,6 +2,7 @@ var round = 0
 var winscore = 0
 var losescore = 0
 var alwaysscore = 0
+var test =0
 
 // 0 ค้อน   1 กรรไกร    2 กระดาษ
 
@@ -35,6 +36,44 @@ function setplay(bot,game){
 }
 
 
+function addloglist(me,bot){
+    //win rock Scissors
+    if(me=="0"&&bot==1){
+        document.getElementById("log-list-page").innerHTML += "<div class='log-list'><img src='/picture/rock.png' class='log-img'><p>Win</p><img src='/picture/Scissors.png' class='log-img'></div>"    
+    }
+    //win Scissors paper
+    else if(me=="1"&&bot==2){
+        document.getElementById("log-list-page").innerHTML += "<div class='log-list' ><img src='/picture/Scissors.png' class='log-img'><p>Win</p><img src='/picture/paper.png' class='log-img'></div>" 
+    }
+    //win paper rock
+    else if(me=="2"&&bot==0){
+        document.getElementById("log-list-page").innerHTML += "<div class='log-list'><img src='/picture/paper.png' class='log-img'><p>Win</p><img src='/picture/rock.png' class='log-img'></div>" 
+    }
+    //draw 
+    else if(bot==me){
+        if(me==0){
+            document.getElementById("log-list-page").innerHTML += "<div class='log-list' ><img src='/picture/rock.png' class='log-img'><p>Draw</p><img src='/picture/rock.png' class='log-img'></div>" 
+        }else if(me==1){
+            document.getElementById("log-list-page").innerHTML += "<div class='log-list' ><img src='/picture/Scissors.png' class='log-img'><p>Draw</p><img src='/picture/Scissors.png' class='log-img'></div>" 
+        }else{
+            document.getElementById("log-list-page").innerHTML += "<div class='log-list'><img src='/picture/paper.png' class='log-img'><p>Draw</p><img src='/picture/paper.png' class='log-img'></div>" 
+        }
+    }
+    //lose rock paper
+    else if(me=="0"&&bot==2){
+        document.getElementById("log-list-page").innerHTML += "<div class='log-list' ><img src='/picture/rock.png' class='log-img'><p>Draw</p><img src='/picture/paper.png' class='log-img'></div>"
+    }
+    //lose Scissors rock
+    else if(me=="1"&&bot==0){
+        document.getElementById("log-list-page").innerHTML += "<div class='log-list' ><img src='/picture/Scissors.png' class='log-img'><p>Draw</p><img src='/picture/rock.png' class='log-img'></div>"
+    }
+    //lose paper Scissors
+    else{
+        document.getElementById("log-list-page").innerHTML += "<div class='log-list' ><img src='/picture/paper.png' class='log-img'><p>Draw</p><img src='/picture/Scissors.png' class='log-img'></div>"
+    }
+}
+
+
 function playGame(game){
     let bot = Math.floor(Math.random()*3)
     if(game=="0"&&bot==1){
@@ -58,7 +97,11 @@ function playGame(game){
     addRound()
    
 document.getElementById("score").innerHTML = "Scoreboard<br>Round : "+round+"<br>Win : "+winscore+"<br>Lose : "+losescore+"<br>Draw : "+alwaysscore
+addloglist(game,bot)
 }
 
 document.getElementById("score").innerHTML = "Scoreboard<br>Round : "+round+"<br>Win : "+winscore+"<br>Lose : "+losescore+"<br>Draw : "+alwaysscore
 document.getElementById("result").innerHTML = "?"
+
+
+
