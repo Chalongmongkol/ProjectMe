ef014edca35b76edae823be28a36521abce5e42a
Chalongmongkol Wankasem <wankasem_c@silpakorn.edu>
2025-02-18 11:06:56 +0700

    updatelog

--- RPSGame/index.js
+++ RPSGame/index.js
@@ -2,7 +2,8 @@ var round = 0
 var winscore = 0
 var losescore = 0
 var alwaysscore = 0
-var test =0
+let loglist = document.getElementById("log-list-page")
+var messagelog = "<h1 style='text-align: center;margin-top: 20%;'>Not Found</h1>"
 
 // 0 ค้อน   1 กรรไกร    2 กระดาษ
 
@@ -16,6 +17,10 @@ function openModal(){
 
 function addRound(){
     round += 1
+    if(round==1){
+        messagelog = ""
+        loglist.innerHTML = messagelog
+    }
 }
 
 function setplay(bot,game){
@@ -37,39 +42,40 @@ function setplay(bot,game){
 
 
 function addloglist(me,bot){
+
     //win rock Scissors
     if(me=="0"&&bot==1){
-        document.getElementById("log-list-page").innerHTML += "<div class='log-list'><img src='/picture/rock.png' class='log-img'><p>Win</p><img src='/picture/Scissors.png' class='log-img'></div>"    
+        loglist.innerHTML += "<div class='log-list'><p>R"+round+"</p><img src='/picture/rock.png' class='log-img'><p>Win</p><img src='/picture/Scissors.png' class='log-img'></div>"    
     }
     //win Scissors paper
     else if(me=="1"&&bot==2){
-        document.getElementById("log-list-page").innerHTML += "<div class='log-list' ><img src='/picture/Scissors.png' class='log-img'><p>Win</p><img src='/picture/paper.png' class='log-img'></div>" 
+        loglist.innerHTML += "<div class='log-list' ><p>R"+round+"</p><img src='/picture/Scissors.png' class='log-img'><p>Win</p><img src='/picture/paper.png' class='log-img'></div>" 
     }
     //win paper rock
     else if(me=="2"&&bot==0){
-        document.getElementById("log-list-page").innerHTML += "<div class='log-list'><img src='/picture/paper.png' class='log-img'><p>Win</p><img src='/picture/rock.png' class='log-img'></div>" 
+        loglist.innerHTML += "<div class='log-list'><p>R"+round+"</p><img src='/picture/paper.png' class='log-img'><p>Win</p><img src='/picture/rock.png' class='log-img'></div>" 
     }
     //draw 
     else if(bot==me){
         if(me==0){
-            document.getElementById("log-list-page").innerHTML += "<div class='log-list' ><img src='/picture/rock.png' class='log-img'><p>Draw</p><img src='/picture/rock.png' class='log-img'></div>" 
+            loglist.innerHTML += "<div class='log-list' ><p>R"+round+"</p><img src='/picture/rock.png' class='log-img'><p>Draw</p><img src='/picture/rock.png' class='log-img'></div>" 
         }else if(me==1){
-            document.getElementById("log-list-page").innerHTML += "<div class='log-list' ><img src='/picture/Scissors.png' class='log-img'><p>Draw</p><img src='/picture/Scissors.png' class='log-img'></div>" 
+            loglist.innerHTML += "<div class='log-list' ><p>R"+round+"</p><img src='/picture/Scissors.png' class='log-img'><p>Draw</p><img src='/picture/Scissors.png' class='log-img'></div>" 
         }else{
-            document.getElementById("log-list-page").innerHTML += "<div class='log-list'><img src='/picture/paper.png' class='log-img'><p>Draw</p><img src='/picture/paper.png' class='log-img'></div>" 
+            loglist.innerHTML += "<div class='log-list'><p>R"+round+"</p><img src='/picture/paper.png' class='log-img'><p>Draw</p><img src='/picture/paper.png' class='log-img'></div>" 
         }
     }
     //lose rock paper
     else if(me=="0"&&bot==2){
-        document.getElementById("log-list-page").innerHTML += "<div class='log-list' ><img src='/picture/rock.png' class='log-img'><p>Lose</p><img src='/picture/paper.png' class='log-img'></div>"
+        loglist.innerHTML += "<div class='log-list' ><p>R"+round+"</p><img src='/picture/rock.png' class='log-img'><p>Lose</p><img src='/picture/paper.png' class='log-img'></div>"
     }
     //lose Scissors rock
     else if(me=="1"&&bot==0){
-        document.getElementById("log-list-page").innerHTML += "<div class='log-list' ><img src='/picture/Scissors.png' class='log-img'><p>Lose</p><img src='/picture/rock.png' class='log-img'></div>"
+        loglist.innerHTML += "<div class='log-list' ><p>R"+round+"</p><img src='/picture/Scissors.png' class='log-img'><p>Lose</p><img src='/picture/rock.png' class='log-img'></div>"
     }
     //lose paper Scissors
     else{
-        document.getElementById("log-list-page").innerHTML += "<div class='log-list' ><img src='/picture/paper.png' class='log-img'><p>Lose</p><img src='/picture/Scissors.png' class='log-img'></div>"
+        loglist.innerHTML += "<div class='log-list' ><p>R"+round+"</p><img src='/picture/paper.png' class='log-img'><p>Lose</p><img src='/picture/Scissors.png' class='log-img'></div>"
     }
 }
 
@@ -78,20 +84,20 @@ function playGame(game){
     let bot = Math.floor(Math.random()*3)
     if(game=="0"&&bot==1){
         winscore += 1
-        document.getElementById("result").innerHTML = "Win"
+        document.getElementById("result").innerHTML = "<p class='result-p' style='color: yellow;'>Win</p>"
         
     }else if(game=="1"&&bot==2){
         winscore += 1
-        document.getElementById("result").innerHTML = "Win"
+        document.getElementById("result").innerHTML = "<p class='result-p' style='color: yellow;'>Win</p>"
     }else if(game=="2"&&bot==0){
         winscore += 1
-        document.getElementById("result").innerHTML = "Win"
+        document.getElementById("result").innerHTML = "<p class='result-p' style='color: yellow;'>Win</p>"
     }else if(game==bot){
         alwaysscore += 1
-        document.getElementById("result").innerHTML = "Draw"
+        document.getElementById("result").innerHTML = "<p class='result-p' style='color: rgb(107, 107, 219);'>Draw</p>"
     }else{
         losescore += 1
-        document.getElementById("result").innerHTML = "lose"
+        document.getElementById("result").innerHTML = "<p class='result-p' style='color:rgb(255, 9, 9);'>Lose</p>"
     } 
     setplay(bot,game)
     addRound()
@@ -101,7 +107,9 @@ addloglist(game,bot)
 }
 
 document.getElementById("score").innerHTML = "Scoreboard<br>Round : "+round+"<br>Win : "+winscore+"<br>Lose : "+losescore+"<br>Draw : "+alwaysscore
-document.getElementById("result").innerHTML = "?"
+document.getElementById("result").innerHTML = "<p class='result-p' >?</p>"
+loglist.innerHTML = messagelog
+
 
 
 
